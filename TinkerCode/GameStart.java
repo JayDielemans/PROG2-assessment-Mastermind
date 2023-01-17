b3fe9e4d584dfd9782424601d3d8b02a6577e703
Jay Dielemans <Jay.dielemans@student.avans.nl>
2023-01-17 20:47:36 +0100

    Code for the message at the start of the game.

---
+++ TinkerCode/GameStart.java
@@ -0,0 +1,20 @@
+package TinkerCode;
+
+public class GameStart {
+
+    public void Start(){
+        System.out.println("=========================================================");
+        System.out.println("| Welkom bij Mastermind!                                |");
+        System.out.println("=========================================================");
+        System.out.println("| Je gaat tegen de computer spelen.                     |");
+        System.out.println("| Het doel is zo snel mogelijk een geheime code raden.  |");
+        System.out.println("| De code bestaat uit 4 letters.                        |");
+        System.out.println("| Mogelijke letters zijn: A, B, C, D, E, F              |");
+        System.out.println("| Een letter kan 0 t/m 4 keer voorkomen in de code.     |");
+        System.out.println("| Jij raadt de code van de computer en andersom.        |");
+        System.out.println("| Wie met het minste pogingen de code raadt wint.       |");
+        System.out.println("| Bij een gelijkspel spelen we nog een ronde.           |");
+        System.out.println("=========================================================");
+    }
+}
+
